bce3b7ae9573f7d669c807891c2f789a090ccc49
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-18 00:56:17 +0300

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import logo from '../../../public/icons/logo.svg';
 
 export const Logo: FC = () => {
   return (
-    <div className="w-[243px] h-[173px] hidden logo-container md:flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className=" hidden logo-container md:flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <Image
         src={logo}
         alt="Company Unixeven Logo"
