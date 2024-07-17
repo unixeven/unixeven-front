60fdaf5ce5271d5f92c8885a338f05fbe92df50e
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-18 00:54:36 +0300

    style: add md styles for WorkingProcess component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,8 +4,14 @@ import logo from '../../../public/icons/logo.svg';
 
 export const Logo: FC = () => {
   return (
-    <div className=" hidden logo-container md:flex items-center justify-center">
-      <Image src={logo} alt="Company Unixeven Logo" width={243} height={173} />
+    <div className="w-[243px] h-[173px] hidden logo-container md:flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <Image
+        src={logo}
+        alt="Company Unixeven Logo"
+        width={243}
+        height={173}
+        className="w-[243px] h-[173px]"
+      />
     </div>
   );
 };
--- src/components/WorkingProcessList/WorkingProcessList.tsx
+++ src/components/WorkingProcessList/WorkingProcessList.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
+import clsx from 'clsx';
 import workProcesses from '../../data/workProcesses.json';
+import { Logo } from '../Logo/Logo';
 
 interface Detail {
   [key: string]: string | undefined;
@@ -14,17 +16,30 @@ interface WorkProcess {
 
 export const WorkingProcessList: FC = () => {
   return (
-    <ul className="grid grid-cols-2 gap-4">
-      {workProcesses.map((process: WorkProcess) => (
+    <ul className="grid grid-cols-2  gap-4 md:gap-0 md:w-full relative">
+      {workProcesses.map((process: WorkProcess, index: number) => (
         <li
           key={process.id}
-          className="gradient-border flex justify-center items-center"
+          className={clsx(
+            'gradient-border relative size-[171px] flex justify-center items-center',
+            {
+              'md:col-span-2 md:size-[162px] md:mx-auto':
+                index === 2 || index === 7,
+              'md:size-[221px] md:justify-self-start':
+                index === 0 || index === 8,
+              'md:size-[221px] md:justify-self-end': index === 1 || index === 9,
+              'md:size-[162px] md:mx-auto':
+                index === 3 || index === 4 || index === 5 || index === 6,
+              'md:mb-[173px]': index === 4,
+            }
+          )}
         >
-          <p className="text-balance w-full font-montserrat text-xl/5 text-center font-bold tracking-[0.6px]">
+          <p className="text-balance w-full font-montserrat text-xl/5 md:text-2xl/6 md:font-medium text-center font-bold tracking-[0.6px]">
             {process.name}
           </p>
         </li>
       ))}
+      <Logo />
     </ul>
   );
 };
