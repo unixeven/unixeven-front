892e9b01aa08b85db9d95fd0bfcea226312b16dc
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-17 16:01:09 +0300

    Update WorkingProcess.tsx

--- src/components/WorkingProcess/WorkingProcess.tsx
+++ src/components/WorkingProcess/WorkingProcess.tsx
@@ -4,7 +4,7 @@ import { WorkingProcessList } from '../WorkingProcessList/WorkingProcessList';
 
 export const WorkingProcess = () => {
   return (
-    <section className="flex flex-col items-center gap-4 h-[1143px] pt-16 pb-[104px] px-4 md:px-6 md:py-20 desk:px-24 bg-workingProcess-bg bg-cover ">
+    <section className="flex flex-col items-center gap-4 h-[1143px] pt-16 pb-[104px] px-4 md:px-6 md:py-20 desk:py-[88px] desk:px-24 bg-workingProcess-bg bg-cover ">
       <TitleSections text="WORKING PROCESS" />
       <WorkingProcessList />
     </section>
