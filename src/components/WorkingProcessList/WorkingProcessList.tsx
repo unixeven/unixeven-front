e2a3d24532521a2e6f4509208e1d7ea8282954b2
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-19 01:29:14 +0300

    style: add background for md and desk screens

--- src/components/WorkingProcessList/WorkingProcessList.tsx
+++ src/components/WorkingProcessList/WorkingProcessList.tsx
@@ -16,34 +16,36 @@ interface WorkProcess {
 
 export const WorkingProcessList: FC = () => {
   return (
-    <ul className="grid grid-cols-2 gap-4 md:gap-0 md:w-full relative xl:transform xl:-rotate-90 xl:w-max">
+    <ul className="grid grid-cols-2 gap-4 md:gap-0 md:w-full relative desk:transform desk:-rotate-90 desk:w-max">
       {workProcesses.map((process: WorkProcess, index: number) => (
         <li
           key={process.id}
           className={clsx(
             'gradient-border relative size-[171px] flex justify-center items-center',
             {
-              'md:col-span-2 md:size-[162px] md:mx-auto':
+              'md:col-span-2 md:size-[162px] md:mx-auto ':
                 index === 2 || index === 7,
-              'md:size-[221px] md:justify-self-start xl:justify-self-center xl:mr-[81px]':
+              'md:size-[221px] md:justify-self-start desk:justify-self-center desk:mr-[81px] desk:-mb-10':
                 index === 0,
-              'md:size-[221px] md:justify-self-end xl:justify-self-center xl:ml-[81px]':
+              'md:size-[221px] md:justify-self-end desk:justify-self-center desk:ml-[81px] desk:-mb-10':
                 index === 1,
-              'md:size-[221px] md:justify-self-start xl:justify-self-start  ':
+              'md:size-[221px] md:justify-self-start desk:justify-self-start desk:-mt-10 ':
                 index === 8,
-              'md:size-[221px] md:justify-self-end xl:justify-self-end':
+              'md:size-[221px] md:justify-self-end desk:justify-self-end desk:-mt-10':
                 index === 9,
-              'md:size-[162px] md:mx-auto xl:mx-0 xl:justify-self-end xl:mr-[81px]':
+              'md:size-[162px] md:mx-auto desk:mx-0 desk:justify-self-end desk:mr-[81px]':
                 index === 3,
-              'md:size-[162px] md:mx-auto xl:mx-0 xl:ml-[81px]': index === 4,
-              'md:size-[162px] md:mx-auto xl:mx-0 xl:justify-self-end xl:mr-[81px] ':
+              'md:size-[162px] md:mx-auto desk:mx-0 desk:ml-[81px]':
+                index === 4,
+              'md:size-[162px] md:mx-auto desk:mx-0 desk:justify-self-end desk:mr-[81px] ':
                 index === 5,
-              'md:size-[162px] md:mx-auto xl:mx-0 xl:ml-[81px]  ': index === 6,
-              'md:mb-[173px] xl:mb-[236px]': index === 4,
+              'md:size-[162px] md:mx-auto desk:mx-0 desk:ml-[81px]  ':
+                index === 6,
+              'md:mb-[173px] desk:mb-[236px]': index === 4,
             }
           )}
         >
-          <p className="xl:transform xl:rotate-90 xl:origin-center text-balance w-full font-montserrat text-xl/5 md:text-2xl/6 md:font-medium text-center font-bold tracking-[0.6px]">
+          <p className="desk:transform desk:rotate-90 desk:origin-center text-balance w-full font-montserrat text-xl/5 md:text-2xl/6 md:font-medium text-center font-bold tracking-[0.6px]">
             {process.name}
           </p>
         </li>
