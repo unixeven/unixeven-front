9ae686c8234956ff999743a2deed42f51d3d114a
Danylo Diachenko <danylodiachenko@gmail.com>
2024-07-19 17:32:30 +0300

    fix Logo, header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-import { AboutUs } from '@/components/AboutUs/AboutUs';
-import { ContactUs } from '@/components/ContactUs/ContactUs';
-import { Container } from '@/components/Container/Container';
-import { Footer } from '@/components/Footer/Footer';
-import { Hero } from '@/components/Hero/Hero';
-import { OurClients } from '@/components/OurClients/OurClients';
-import { OurServices } from '@/components/OurServices/OurServices';
-import { OurTeam } from '@/components/OurTeam/OurTeam';
-import { WorkingProcess } from '@/components/WorkingProcess/WorkingProcess';
+import { AboutUs } from "@/components/AboutUs/AboutUs";
+import { ContactUs } from "@/components/ContactUs/ContactUs";
+import { Container } from "@/components/Container/Container";
+import { Footer } from "@/components/Footer/Footer";
+import { Header } from "@/components/Header/Header";
+import { Hero } from "@/components/Hero/Hero";
+import { OurClients } from "@/components/OurClients/OurClients";
+import { OurServices } from "@/components/OurServices/OurServices";
+import { OurTeam } from "@/components/OurTeam/OurTeam";
+import { WorkingProcess } from "@/components/WorkingProcess/WorkingProcess";
 
 export default function Home() {
   return (
     <main>
+      {/* <div className="bg-hero-bg-up  bg-no-repeat"> */}
+      <Header />
       <Hero />
+      {/* </div> */}
       <AboutUs />
       <OurTeam />
       <OurServices />
--- src/components/WorkingLogo/WorkingLogo.tsx
+++ src/components/WorkingLogo/WorkingLogo.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
-import { Logo } from '../Logo/Logo';
+import { Logo } from "../Logo/Logo";
 
 export const WorkingLogo: FC = () => {
   return (
     <div className="  hidden logo-container md:flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Logo />
+      <Logo width={203} height={173} />
     </div>
   );
 };
