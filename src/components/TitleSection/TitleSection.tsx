a3b99a3b9890b65680646ec32a13f65230b69475
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-19 08:44:28 +0300

    style: update desk styles for WorkingProcess component

--- src/components/TitleSection/TitleSection.tsx
+++ src/components/TitleSection/TitleSection.tsx
@@ -6,7 +6,7 @@ interface TitleSectionsProps {
 
 export const TitleSections: FC<TitleSectionsProps> = ({ text }) => {
   return (
-    <h2 className="title-section gradient-text relative  w-full text-center md:text-left font-dmSerif text-4xl tracking-[0.5px] font-normal md:pl-[106px] ">
+    <h2 className="title-section gradient-text relative  w-full text-center md:text-left font-dmSerif text-4xl tracking-[0.5px] font-normal md:pl-[106px] uppercase ">
       {text}
     </h2>
   );
