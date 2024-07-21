bfad8246b076ccbd2d0ae24295109c38ebc63f30
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-21 21:49:42 +0300

    style: updatw styles footer

--- src/components/ContactInfo/ContactInfo.tsx
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -8,22 +8,22 @@ export const ContactInfo = () => {
       <FooterTitle text="Contact us" />
       <ul className="flex flex-col gap-2 text-lg font-normal">
         <li className="flex items-center gap-2">
-          <FiPhoneCall size={36} />
           <a
             href="tel:+380967845734"
-            className="transition-colors duration-300 dark:hover:text-pacificBlue hover:text-lightWhite focus:text-lightWhite dark:focus:text-pacificBlue focus:outline-none"
+            className="flex items-center gap-2 transition-colors duration-300 dark:hover:text-pacificBlue hover:text-lightWhite focus:text-lightWhite dark:focus:text-pacificBlue focus:outline-none"
             aria-label="Call us at +380967845734"
           >
+            <FiPhoneCall size={36} />
             +380967845734
           </a>
         </li>
         <li className="flex items-center gap-2">
-          <FiMail size={36} />
           <a
             href="mailto:email458678@gmail.com"
-            className="transition-colors duration-300 dark:hover:text-pacificBlue hover:text-lightWhite focus:text-lightWhite dark:focus:text-pacificBlue focus:outline-none"
+            className="flex items-center gap-2 transition-colors duration-300 dark:hover:text-pacificBlue hover:text-lightWhite focus:text-lightWhite dark:focus:text-pacificBlue focus:outline-none"
             aria-label="Email us at email458678@gmail.com"
           >
+            <FiMail size={36} />
             email458678@gmail.com
           </a>
         </li>
