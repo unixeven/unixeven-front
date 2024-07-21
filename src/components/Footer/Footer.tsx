041f0d66bce17308a79301bb86ec63aed6a1e2ad
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-21 19:02:56 +0300

    feat(footer): add ServicesNavigation, OnlineChat, ContactInfo components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Logo } from '../Logo/Logo';
+import { ServicesNavigation } from '../ServicesNavigation/ServicesNavigation';
+import { OnlineChat } from '../OnlineChat/OnlineChat';
+import { ContactInfo } from '../ContactInfo/ContactInfo';
 
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="px-12 py-16 flex flex-col justify-start items-center gap-14 font-montserrat dark:text-lightWhite text-prussianBlue">
+      <Logo width={82} height={73} />
+      <ServicesNavigation />
+      <OnlineChat />
+      <ContactInfo />
+    </footer>
+  );
 };
---
+++ src/components/OnlineChat/OnlineChat.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaTelegramPlane, FaViber } from 'react-icons/fa';
+import { FooterTitle } from '../FooterTitle/FooterTitle';
+
+export const OnlineChat = () => {
+  return (
+    <div className="flex flex-col gap-6 self-start">
+      <FooterTitle text="Online chat" />
+      <ul className="flex flex-col gap-2 text-lg/normal font-normal">
+        <li className="flex items-center gap-2">
+          <a
+            href="https://t.me/yourUsername"
+            className="flex items-center hover:text-gray-300"
+          >
+            <FaTelegramPlane size={36} className="mr-2" />
+            Telegram
+          </a>
+        </li>
+        <li className="flex items-center gap-2">
+          <a
+            href="viber://chat?number=+YourNumber"
+            className="flex items-center hover:text-gray-300"
+          >
+            <FaViber size={36} className="mr-2" />
+            Viber
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
