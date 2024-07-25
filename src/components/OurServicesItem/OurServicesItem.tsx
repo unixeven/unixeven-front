bb52115a1c202ce4d4eac6d8bd622e69c008c22d
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-25 10:54:07 +0300

    feat: add dynamic icon loading from sprite

--- src/components/OurServicesItem/OurServicesItem.tsx
+++ src/components/OurServicesItem/OurServicesItem.tsx
@@ -24,16 +24,16 @@ export const OurServicesItem: FC<OurServicesItemProps> = ({
       key={service.id}
       className="w-full  desk:w-[calc((100%-2*24px)/3)] gradient-border  dark:border-0  border-2 border-cobalt relative px-6 pb-6 pt-[46px] md:px-10 md:pt-[109px] md:pb-10 desk:px-6 desk:pt-[46px] desk:pb-6 flex flex-col items-center  justify-center gap-4 !rounded-[32px] before:!rounded-[32px] transition-all duration-300 hover:border-pacificBlue hover:shadow-custom-black "
     >
-      <h3 className="uppercase font-montserrat text-[25px]/[40px] font-medium tracking-[0.5px] dark:text-lightWhite text-cobalt">
+      <h3 className="uppercase font-montserrat text-[25px]/[40px] font-medium tracking-[0.5px] dark:text-lightWhite text-prussianBlue">
         {service.title}
       </h3>
-      <Image
-        src="/icons/qa.svg"
-        alt="Company Unixeven Logo"
-        width={80}
-        height={47}
-      />
 
+      <svg aria-hidden="true" className="w-20 h-12">
+        <use
+          href={`/icons/sprite.svg#icon-${service.icon}`}
+          className="size-full fill-prussianBlue dark:fill-lightWhite"
+        />
+      </svg>
       <button
         id="open-modal"
         onClick={() => handleOpenModal(service)}
