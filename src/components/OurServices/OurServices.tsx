64fbb8c25e73500c53b3ef5454867117f16a108f
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-25 17:37:48 +0300

    feat: added accessibility improvements to OurServices components

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -4,8 +4,11 @@ import { OurServicesList } from '../OurServicesList/OurServicesList';
 
 export const OurServices = () => {
   return (
-    <section className="flex flex-col items-center gap-14 md:gap-[71px] desk:gap-16 w-full px-4 py-16 md:px-[113px] md:py-20 desk:px-24 desk:py-[88px] bg-ourServices-bg md:bg-ourServices-bg-md desk:bg-ourServices-bg-desk bg-contain bg-ourServicesPosition md:bg-[center_bottom_4rem] desk:bg-bottom  bg-no-repeat">
-      <TitleSections text="Our Services" id="Our Services" />
+    <section
+      aria-labelledby="our-services-title"
+      className="flex flex-col items-center gap-14 md:gap-[71px] desk:gap-16 w-full px-4 py-16 md:px-[113px] md:py-20 desk:px-24 desk:py-[88px] bg-ourServices-bg md:bg-ourServices-bg-md desk:bg-ourServices-bg-desk bg-contain bg-ourServicesPosition md:bg-[center_bottom_4rem] desk:bg-bottom  bg-no-repeat"
+    >
+      <TitleSections text="Our Services" id="our-services-title" />
       <OurServicesList />
     </section>
   );
