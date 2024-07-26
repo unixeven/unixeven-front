d5e72d1b5f8d1741a8ae8d0e0f7f3095b19dbf7a
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-26 15:22:25 +0300

    feat(fotter): add smooth scrolling and focus functionality from footer to service items

--- src/components/ServicesNavigation/ServicesNavigation.tsx
+++ src/components/ServicesNavigation/ServicesNavigation.tsx
@@ -1,18 +1,42 @@
-import services from '../../data/ourServices.json';
+'use client';
 
+import services from '../../data/ourServices.json';
 import Link from 'next/link';
 import { FooterTitle } from '../FooterTitle/FooterTitle';
+import { scroller } from 'react-scroll';
 
-export const ServicesNavigation = () => {
+export const ServicesNavigation: React.FC = () => {
+  const handleScrollToServices = (index: number) => {
+    const offset = index === 3 || index === 4 ? -550 : -300;
+
+    scroller.scrollTo(`service-button-${index}`, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+      offset,
+    });
+
+    const button = document.getElementById(
+      `service-button-${index}`
+    ) as HTMLButtonElement;
+    if (button) {
+      button.focus();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-6 ">
+    <div className="flex flex-col gap-6">
       <FooterTitle text="Services" />
       <nav className="">
         <ul className="flex flex-col gap-4">
-          {services.map(service => (
+          {services.map((service, index) => (
             <li key={service.id} className="text-lg/normal font-normal">
               <Link
-                href="#"
+                href={`#service-button-${index}`}
+                onClick={e => {
+                  e.preventDefault();
+                  handleScrollToServices(index);
+                }}
                 className="transition-colors duration-300 dark:hover:text-pacificBlue hover:text-lightWhite focus:text-lightWhite dark:focus:text-pacificBlue focus:outline-none"
               >
                 {service.title}
