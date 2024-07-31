92f0d77960e035ddcf30401a169f30015e133ab9
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-31 11:32:07 +0300

    fix: fix scroll hide/show in burger menu

--- src/components/HeaderBurgerMenu/BurgerMenu.tsx
+++ src/components/HeaderBurgerMenu/BurgerMenu.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { FC, useState } from 'react';
-// import { GiHamburgerMenu } from 'react-icons/gi';
+
 import { NavBar } from '../HeaderNavBar/NavBar';
-import { RxCross1, RxHamburgerMenu } from 'react-icons/rx';
-import { CgClose } from 'react-icons/cg';
-import { TfiClose } from 'react-icons/tfi';
+import { RxCross1 } from 'react-icons/rx';
 
 interface BurgerMenuProps {
   nav: { name: string; id: string }[];
@@ -13,13 +11,14 @@ interface BurgerMenuProps {
 
 export const BurgerMenu: FC<BurgerMenuProps> = ({ nav }) => {
   const [isOpen, setIsOpen] = useState(false);
+  console.log(`isOpen:`, isOpen);
 
   const toggleMenu = () => {
     const body = document.body;
     const newOverflow = body.style.overflow === 'hidden' ? 'auto' : 'hidden';
 
     body.style.overflow = newOverflow;
-    setIsOpen((prev) => !prev);
+    setIsOpen(prev => !prev);
   };
   return (
     <div className="block xl:hidden">
@@ -35,7 +34,7 @@ export const BurgerMenu: FC<BurgerMenuProps> = ({ nav }) => {
             className="absolute top-10 right-4 dark:text-lightWhite text-prussianBlue"
           />
           <div className="flex flex-col items-center gap-20 mt-[215px]">
-            <NavBar nav={nav} />
+            <NavBar nav={nav} toggleMenu={toggleMenu} />
           </div>
         </div>
       )}
