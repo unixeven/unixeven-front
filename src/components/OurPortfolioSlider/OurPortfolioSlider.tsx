147880fb0d50c39db2130b63693afba2e3d29830
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-31 22:28:27 +0300

    Update OurPortfolioSlider.tsx

--- src/components/OurPortfolioSlider/OurPortfolioSlider.tsx
+++ src/components/OurPortfolioSlider/OurPortfolioSlider.tsx
@@ -3,27 +3,37 @@ import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-
 import 'swiper/css/effect-coverflow';
 import { Navigation, EffectCoverflow, Autoplay } from 'swiper/modules';
 import Image from 'next/image';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 export const Slider: FC = () => {
   return (
-    <div className="collection">
+    <div className="collection relative">
+      <button className="prev size-[42px] flex justify-center items-center z-10 border-2 border-lightWhite rounded-full absolute bottom-2/4 left-0 cursor-pointer hover:scale-110 focus:scale-110 transition-transform ">
+        <IoIosArrowBack className="size-8  fill-lightWhite " />
+      </button>
+      <button className="next size-[42px] flex justify-center items-center z-10 border-2 border-lightWhite rounded-full absolute  bottom-2/4 right-0 cursor-pointer hover:scale-110 focus:scale-110 transition-transform">
+        <IoIosArrowForward className=" size-8 fill-lightWhite" />
+      </button>
       <Swiper
+        className="w-[1036px]"
         modules={[Navigation, EffectCoverflow, Autoplay]}
-        navigation
+        navigation={{
+          prevEl: '.prev',
+          nextEl: '.next',
+        }}
         effect="coverflow"
         grabCursor={true}
         centeredSlides={true}
         loop={true}
         autoHeight={true}
-        slidesPerView="auto"
+        slidesPerView={3}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
-          depth: 150,
+          depth: 160,
           modifier: 2.5,
           slideShadows: true,
         }}
