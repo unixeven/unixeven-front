606b6b8a54bf38a86b2ce097905bc19f7b47011d
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-08-04 02:01:54 +0300

    feat: add target="_blank" and rel="noopener noreferrer" to social media links for security

--- src/components/SocialContactUs/SocialContactUs.tsx
+++ src/components/SocialContactUs/SocialContactUs.tsx
@@ -17,6 +17,8 @@ export const SocialContactUs = () => {
           <Link
             key={media.name}
             href={media.href}
+            target="_blank"
+            rel="noopener noreferrer"
             className="group flex items-center outline-none justify-center gradient-border dark:relative dark:border-0 dark:hover:shadow-none dark:focus:shadow-none dark:hover:before:bg-pacificBlue dark:focus:before:bg-pacificBlue p-4 size-16 rounded-full border-2 border-cobalt cursor-pointer hover:border-pacificBlue focus:border-pacificBlue"
             aria-label={`Visit our ${media.name} page`}
           >
