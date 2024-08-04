1b983c66b75114a2d717daa570aa2deda719f345
Danylo Diachenko <danylodiachenko@gmail.com>
2024-08-04 13:45:44 +0300

    add json form

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -1,11 +1,11 @@
 import { Dictionary } from '@/types/definitions';
-import 'server-only';
+// import 'server-only';
 
 export type Locale = keyof typeof dictionaries;
 
 const dictionaries = {
-  en: () => import('./dictionaries/en.json').then(module => module.default),
-  uk: () => import('./dictionaries/uk.json').then(module => module.default),
+  en: () => import('./dictionaries/en.json').then((module) => module.default),
+  uk: () => import('./dictionaries/uk.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale): Promise<Dictionary> =>
